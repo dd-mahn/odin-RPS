b02497481ac7bf1a8fbff8f9f9dc3d9e870f4ed9
godsadeser <ducmanh0801@gmail.com>
2023-06-19 16:15:31 +0700

    (1) Add pseudocode

---
+++ script.js
@@ -0,0 +1,14 @@
+// Define variables for the game
+// Create function to get computer's choice
+    //Random index from 1 to 3 
+    //Assign each index with R/P/S
+    //Return the index
+// Create function to play a single round
+    //Get user's input (R/P/S)
+    //Make the input case sensitive by lower it's case.
+    //Get computer's choice
+    //Combine
+        //Tie (Using if)
+        //Win or lose (Using switch)
+    //Return the message value
+// Create function to play 5 rounds in a row and return the final winner
