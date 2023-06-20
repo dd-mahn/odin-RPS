30e8f85d219897cfa86e0123bd2cb431f7cac502
godsadeser <ducmanh0801@gmail.com>
2023-06-20 08:43:15 +0700

    Move content to index file

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 // Define variables for the game
+console.log("test");
+let computerSelection;
+let playerSelection;
 // Create function to get computer's choice
+function getComputerChoice(){
     //Random index from 1 to 3 
+    let compIndex = Math.floor(Math.random() * 3);
     //Assign each index with R/P/S
-    //Return the index
+    let choices = ["rock","paper","scissor"];
+    let compChoices = choices[compIndex];
+    //Return the choice
+    return compChoices;
+}
+console.log(getComputerChoice());
 // Create function to play a single round
     //Get user's input (R/P/S)
     //Make the input case sensitive by lower it's case.
